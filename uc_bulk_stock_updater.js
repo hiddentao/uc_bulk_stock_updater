9994f5ac9e4aff3938a75b72f28db63c8839553e
Ramesh Nair <ram@hiddentao.com>
2010-09-03 11:41:30 +0100

    #897862 by hiddentao: Changed how values get updated and shown as editable text fields. The new, more flexible system makes it easy to add more editable fields in future.

--- uc_bulk_stock_updater.js
+++ uc_bulk_stock_updater.js
@@ -1,13 +1,13 @@
-// $Id: uc_bulk_stock_updater.js,v 1.1 2010/08/28 10:45:52 hiddentao Exp $
+// $Id: uc_bulk_stock_updater.js,v 1.2 2010/08/29 18:12:29 hiddentao Exp $
 
 
 Drupal.behaviors.uc_bulk_stock_updater = function (context) {
 	
-	// keep track of which stock levels get edited
+	// handle item editing
 	$("input.uc_bulk_stock_updater_value").each(function(){
 		$(this).data("original", $(this).val());
 		$(this).change(function(){
-			uc_bulk_stock_updater_submitStockValue(this);
+			uc_bulk_stock_updater_submitValue(this);
 		})
 	});
 
@@ -35,47 +35,49 @@ Drupal.behaviors.uc_bulk_stock_updater = function (context) {
 };
 
 
-function uc_bulk_stock_updater_submitStockValue(stockInputElem)
+function uc_bulk_stock_updater_submitValue(inputElem)
 {
-	var _sku = $(stockInputElem).attr("name");
-	var _stock = $(stockInputElem).val();
+	var _parentTR = $($(inputElem).parents("tr").get(0));
+	var _sku = _parentTR.children("td:first").text();
+	var _name = $(inputElem).attr("name");
+	var _value = $(inputElem).val();
 
 	// reset error msgs
-	$(stockInputElem)
+	$(inputElem)
 		.removeClass("error")
 		.after("<div class=\"uc_bulk_stock_updater_ajax_progress\"></div>");
 	
-	$(stockInputElem).nextAll("div.uc_bulk_stock_updater_ajax_error").remove();
+	$(inputElem).nextAll("div.uc_bulk_stock_updater_ajax_error").remove();
 	
 	// call
 	$.ajax({
 		url : Drupal.settings.uc_bulk_stock_updater.ajax_url,
 		type: 'POST',
 		timeout : 3000,
-		data : { sku: _sku, stock: _stock },
+		data : { sku: _sku, name: _name, value: _value },
 		dataType : "json",
 	    error : function(_XMLHttpRequest, _textStatus, _errorThrown)
 	    {
-			uc_bulk_stock_updater_submitStockValueErr(stockInputElem, _textStatus);
+			uc_bulk_stock_updater_submitValueErr(inputElem, _textStatus);
 	    },			
 		success : function(_data, _textStatus, _XMLHttpRequest)
 		{
 	    	if (undefined != _data.error)
-	    		uc_bulk_stock_updater_submitStockValueErr(stockInputElem, _data.error);
+	    		uc_bulk_stock_updater_submitValueErr(inputElem, _data.error);
 	    	else
-	    		$(stockInputElem).data("original", _stock);
+	    		$(inputElem).data("original", _value);
 	    },
 		complete : function(_XMLHttpRequest,_textStatus)
 		{
-			$(stockInputElem).nextAll("div.uc_bulk_stock_updater_ajax_progress").remove();
+			$(inputElem).nextAll("div.uc_bulk_stock_updater_ajax_progress").remove();
 		}	    
 	});	
 }
-function uc_bulk_stock_updater_submitStockValueErr(stockInputElem, _errorMsg)
+function uc_bulk_stock_updater_submitValueErr(inputElem, _errorMsg)
 {
-	$(stockInputElem)
+	$(inputElem)
 		.after("<div class=\"uc_bulk_stock_updater_ajax_error\">" + _errorMsg + "</div>")
-		.val($(stockInputElem).data("original"))
+		.val($(inputElem).data("original"))
 		.addClass("error");
 }
 
